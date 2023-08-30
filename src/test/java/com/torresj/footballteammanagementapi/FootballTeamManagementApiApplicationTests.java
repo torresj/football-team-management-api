2ea6f19ea6dbec675c4e526cef78425962b5a9ca
Jaime Torres <jtbenavente@gmail.com>
2023-08-30 14:12:17 +0200

    First commit with the initial structure

---
+++ src/test/java/com/torresj/footballteammanagementapi/FootballTeamManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torresj.footballteammanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballTeamManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
