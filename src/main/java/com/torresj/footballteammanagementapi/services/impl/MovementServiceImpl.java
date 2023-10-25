cfe3cc817cfdf6ce2c9d88999d89c2d0789c84f3
jaimetorres <jaime.torres@version1.com>
2023-10-25 10:11:47 +0200

    Date format updated

--- src/main/java/com/torresj/footballteammanagementapi/services/impl/MovementServiceImpl.java
+++ src/main/java/com/torresj/footballteammanagementapi/services/impl/MovementServiceImpl.java
@@ -8,7 +8,10 @@ import com.torresj.footballteammanagementapi.exceptions.MovementNotFoundExceptio
 import com.torresj.footballteammanagementapi.repositories.MemberRepository;
 import com.torresj.footballteammanagementapi.repositories.MovementRepository;
 import com.torresj.footballteammanagementapi.services.MovementService;
+
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,134 +19,136 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MovementServiceImpl implements MovementService {
 
-  private final MovementRepository movementRepository;
-  private final MemberRepository memberRepository;
+    private final MovementRepository movementRepository;
+    private final MemberRepository memberRepository;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-  @Override
-  public List<MovementDto> get() {
-    return movementRepository.findAll().stream()
-        .map(
-            entity -> {
-              var member = memberRepository.findById(entity.getMemberId());
-              String memberName =
-                  member
-                      .map(memberEntity -> memberEntity.getName() + " " + memberEntity.getSurname())
-                      .orElse("Not found");
-              return new MovementDto(
-                  entity.getId(),
-                  entity.getType(),
-                  memberName,
-                  entity.getAmount(),
-                  entity.getDescription(),
-                  entity.getCreatedOn());
-            })
-        .toList();
-  }
+    @Override
+    public List<MovementDto> get() {
+        return movementRepository.findAll().stream()
+                .map(
+                        entity -> {
+                            var member = memberRepository.findById(entity.getMemberId());
+                            String memberName =
+                                    member
+                                            .map(memberEntity -> memberEntity.getName() + " " + memberEntity.getSurname())
+                                            .orElse("Not found");
+                            return new MovementDto(
+                                    entity.getId(),
+                                    entity.getType(),
+                                    memberName,
+                                    entity.getAmount(),
+                                    entity.getDescription(),
+                                    formatter.format(entity.getCreatedOn()));
+                        })
+                .toList();
+    }
 
-  @Override
-  public MovementDto get(long id) throws MovementNotFoundException {
-    var movement =
-        movementRepository.findById(id).orElseThrow(() -> new MovementNotFoundException(id));
-    var member = memberRepository.findById(movement.getMemberId());
-    String memberName =
-        member
-            .map(memberEntity -> memberEntity.getName() + " " + memberEntity.getSurname())
-            .orElse("Not found");
+    @Override
+    public MovementDto get(long id) throws MovementNotFoundException {
+        var movement =
+                movementRepository.findById(id).orElseThrow(() -> new MovementNotFoundException(id));
+        var member = memberRepository.findById(movement.getMemberId());
+        String memberName =
+                member
+                        .map(memberEntity -> memberEntity.getName() + " " + memberEntity.getSurname())
+                        .orElse("Not found");
 
-    return new MovementDto(
-        movement.getId(),
-        movement.getType(),
-        memberName,
-        movement.getAmount(),
-        movement.getDescription(),
-        movement.getCreatedOn());
-  }
+        return new MovementDto(
+                movement.getId(),
+                movement.getType(),
+                memberName,
+                movement.getAmount(),
+                movement.getDescription(),
+                formatter.format(movement.getCreatedOn()));
+    }
 
-  @Override
-  public List<MovementDto> getByMember(long memberId) throws MemberNotFoundException {
-    memberRepository.findById(memberId).orElseThrow(() -> new MemberNotFoundException(""));
-    return movementRepository.findByMemberId(memberId).stream()
-        .map(
-            movementEntity -> {
-              var member = memberRepository.findById(movementEntity.getMemberId());
-              String memberName =
-                  member
-                      .map(memberEntity -> memberEntity.getName() + " " + memberEntity.getSurname())
-                      .orElse("Not found");
-              return new MovementDto(
-                  movementEntity.getId(),
-                  movementEntity.getType(),
-                  memberName,
-                  movementEntity.getAmount(),
-                  movementEntity.getDescription(),
-                  movementEntity.getCreatedOn());
-            })
-        .toList();
-  }
-
-  @Override
-  public double getBalance(long memberId) {
-    return movementRepository.findByMemberId(memberId).stream()
-        .mapToDouble(MovementEntity::getAmount)
-        .sum();
-  }
-
-  @Override
-  public MovementDto create(long memberId, MovementType type, double amount, String description)
-      throws MemberNotFoundException {
-    var member =
+    @Override
+    public List<MovementDto> getByMember(long memberId) throws MemberNotFoundException {
         memberRepository.findById(memberId).orElseThrow(() -> new MemberNotFoundException(""));
+        return movementRepository.findByMemberId(memberId).stream()
+                .map(
+                        movementEntity -> {
+                            var member = memberRepository.findById(movementEntity.getMemberId());
+                            String memberName =
+                                    member
+                                            .map(memberEntity -> memberEntity.getName() + " " + memberEntity.getSurname())
+                                            .orElse("Not found");
+                            return new MovementDto(
+                                    movementEntity.getId(),
+                                    movementEntity.getType(),
+                                    memberName,
+                                    movementEntity.getAmount(),
+                                    movementEntity.getDescription(),
+                                    formatter.format(movementEntity.getCreatedOn()));
+                        })
+                .toList();
+    }
 
-    var movementEntity =
-        movementRepository.save(
-            MovementEntity.builder()
-                .type(type)
-                .amount(amount)
-                .memberId(memberId)
-                .description(description)
-                .build());
+    @Override
+    public double getBalance(long memberId) {
+        return movementRepository.findByMemberId(memberId).stream()
+                .mapToDouble(MovementEntity::getAmount)
+                .sum();
+    }
 
-    return new MovementDto(
-        movementEntity.getId(),
-        movementEntity.getType(),
-        member.getName() + " " + member.getSurname(),
-        movementEntity.getAmount(),
-        movementEntity.getDescription(),
-        movementEntity.getCreatedOn());
-  }
+    @Override
+    public MovementDto create(long memberId, MovementType type, double amount, String description)
+            throws MemberNotFoundException {
+        var member =
+                memberRepository.findById(memberId).orElseThrow(() -> new MemberNotFoundException(""));
 
-  @Override
-  public MovementDto update(long id, double amount, String description)
-      throws MovementNotFoundException, MemberNotFoundException {
-    var movement =
-        movementRepository.findById(id).orElseThrow(() -> new MovementNotFoundException(id));
+        var movementEntity =
+                movementRepository.save(
+                        MovementEntity.builder()
+                                .type(type)
+                                .amount(amount)
+                                .memberId(memberId)
+                                .description(description)
+                                .build());
 
-    var member =
-        memberRepository
-            .findById(movement.getMemberId())
-            .orElseThrow(() -> new MemberNotFoundException(""));
+        return new MovementDto(
+                movementEntity.getId(),
+                movementEntity.getType(),
+                member.getName() + " " + member.getSurname(),
+                movementEntity.getAmount(),
+                movementEntity.getDescription(),
+                formatter.format(movementEntity.getCreatedOn()));
+    }
 
-    var movementUpdated =
-        movementRepository.save(
-            MovementEntity.builder()
-                .id(movement.getId())
-                .type(movement.getType())
-                .amount(amount)
-                .memberId(movement.getMemberId())
-                .description(description)
-                .build());
+    @Override
+    public MovementDto update(long id, double amount, String description)
+            throws MovementNotFoundException, MemberNotFoundException {
+        var movement =
+                movementRepository.findById(id).orElseThrow(() -> new MovementNotFoundException(id));
 
-    return new MovementDto(
-        movementUpdated.getId(),
-        movementUpdated.getType(),
-        member.getName() + " " + member.getSurname(),
-        movementUpdated.getAmount(),
-        movementUpdated.getDescription(),
-        movementUpdated.getCreatedOn());
-  }
+        var member =
+                memberRepository
+                        .findById(movement.getMemberId())
+                        .orElseThrow(() -> new MemberNotFoundException(""));
 
-  @Override
-  public void delete(long id) {
-    movementRepository.deleteById(id);
-  }
+        var movementUpdated =
+                movementRepository.save(
+                        MovementEntity.builder()
+                                .id(movement.getId())
+                                .type(movement.getType())
+                                .amount(amount)
+                                .memberId(movement.getMemberId())
+                                .description(description)
+                                .createdOn(movement.getCreatedOn())
+                                .build());
+
+        return new MovementDto(
+                movementUpdated.getId(),
+                movementUpdated.getType(),
+                member.getName() + " " + member.getSurname(),
+                movementUpdated.getAmount(),
+                movementUpdated.getDescription(),
+                formatter.format(movementUpdated.getCreatedOn()));
+    }
+
+    @Override
+    public void delete(long id) {
+        movementRepository.deleteById(id);
+    }
 }
