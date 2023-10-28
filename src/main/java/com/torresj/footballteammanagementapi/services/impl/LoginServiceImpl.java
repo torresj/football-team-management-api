44918e733b17e4c25a7ef973c1b614cfb3cc4f04
jaimetorres <jaime.torres@version1.com>
2023-10-28 19:14:56 +0200

    injured error fixed in login

--- src/main/java/com/torresj/footballteammanagementapi/services/impl/LoginServiceImpl.java
+++ src/main/java/com/torresj/footballteammanagementapi/services/impl/LoginServiceImpl.java
@@ -16,45 +16,46 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class LoginServiceImpl implements LoginService {
 
-  private final MemberRepository memberRepository;
-  private final JwtService jwtService;
-  private final PasswordEncoder encoder;
+    private final MemberRepository memberRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder encoder;
 
-  @Override
-  public ResponseLoginDto login(String userName, String password, long nonce)
-      throws MemberNotFoundException {
-    log.debug("[LOGIN] Finding user");
-    if (userName.split("\\.").length != 2) {
-      throw new MemberNotFoundException(userName);
+    @Override
+    public ResponseLoginDto login(String userName, String password, long nonce)
+            throws MemberNotFoundException {
+        log.debug("[LOGIN] Finding user");
+        if (userName.split("\\.").length != 2) {
+            throw new MemberNotFoundException(userName);
+        }
+
+        var member =
+                memberRepository
+                        .findByNameAndSurname(userName.split("\\.")[0], userName.split("\\.")[1])
+                        .orElseThrow(() -> new MemberNotFoundException(userName));
+
+        if (!encoder.matches(password, member.getPassword()))
+            throw new MemberNotFoundException(userName);
+
+        if (member.getNonce() >= nonce) {
+            log.warn("[LOGIN] Error. Nonce already used");
+            throw new MemberNotFoundException(userName);
+        }
+
+        memberRepository.save(
+                MemberEntity.builder()
+                        .id(member.getId())
+                        .name(member.getName())
+                        .surname(member.getSurname())
+                        .password(member.getPassword())
+                        .phone(member.getPhone())
+                        .role(member.getRole())
+                        .nCaptaincies(member.getNCaptaincies())
+                        .nonce(nonce)
+                        .injured(member.isInjured())
+                        .build());
+
+        log.debug("[LOGIN] Login success. Generating JWT ...");
+        String jwt = jwtService.createJWS(userName);
+        return new ResponseLoginDto(jwt);
     }
-
-    var member =
-        memberRepository
-            .findByNameAndSurname(userName.split("\\.")[0], userName.split("\\.")[1])
-            .orElseThrow(() -> new MemberNotFoundException(userName));
-
-    if (!encoder.matches(password, member.getPassword()))
-      throw new MemberNotFoundException(userName);
-
-    if (member.getNonce() >= nonce) {
-      log.warn("[LOGIN] Error. Nonce already used");
-      throw new MemberNotFoundException(userName);
-    }
-
-    memberRepository.save(
-        MemberEntity.builder()
-            .id(member.getId())
-            .name(member.getName())
-            .surname(member.getSurname())
-            .password(member.getPassword())
-            .phone(member.getPhone())
-            .role(member.getRole())
-            .nCaptaincies(member.getNCaptaincies())
-            .nonce(nonce)
-            .build());
-
-    log.debug("[LOGIN] Login success. Generating JWT ...");
-    String jwt = jwtService.createJWS(userName);
-    return new ResponseLoginDto(jwt);
-  }
 }
